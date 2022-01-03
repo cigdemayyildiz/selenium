13adb0d3b534846d0968fd222b809e985b67bef2
Cigdem <cigdemrespect@gmail.com>
2022-01-03 14:51:35 -0600

    SweetAlert

--- src/main/java/alert/SweetAlert.java
+++ src/main/java/alert/SweetAlert.java
@@ -6,14 +6,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class SweetAlert {
 
     @Test
     public void sweetAlert() throws InterruptedException {
+
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://sweetalert.js.org/");
